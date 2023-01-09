2bbcc6176007740e1de631421ea422e1907f4139
VishalChauhan525 <vishalchauhan525@gmail.com>
2023-01-09 16:39:33 +0530

    build commit

---
+++ dist/assets/index.58a9994a.js
@@ -0,0 +1,189 @@
+import{o as s,ae as d,j as o,af as e,d3 as a,d4 as t,d5 as i,cp as u,r as n,cj as T,ah as l}from"./index.b8b13c58.js";import{C as h}from"./index.9b59a91a.js";import{B as m}from"./index.4c823db6.js";import"./App.e1b9a34b.js";const y="/assets/transparent.7400a7a9.svg",c=o("button",{type:"button",className:"ms-1 btn-close"}),p=()=>s(d,{children:[o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-primary my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a primary background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-secondary my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a secondary background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-success my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a success background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-danger my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a danger background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-warning my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a warning background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-info my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on an info background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 bg-dark my-2 rounded",children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on an dark background \u2014 check it out!"})]})})}),o(e,{md:"6",sm:"12",children:o("div",{className:"p-3 my-2 rounded",style:{background:`url(${y})`},children:s(a,{children:[o(t,{close:c,children:"Vuexy"}),o(i,{children:"This is a toast on a transparent background \u2014 check it out!"})]})})})]}),g=s("svg",{viewBox:"0 0 139 95",version:"1.1",height:"14",children:[s("defs",{children:[s("linearGradient",{x1:"100%",y1:"10.5120544%",x2:"50%",y2:"89.4879456%",id:"linearGradient-1",children:[o("stop",{stopColor:"#000000",offset:"0%"}),o("stop",{stopColor:"#FFFFFF",offset:"100%"})]}),s("linearGradient",{x1:"64.0437835%",y1:"46.3276743%",x2:"37.373316%",y2:"100%",id:"linearGradient-2",children:[o("stop",{stopColor:"#EEEEEE",stopOpacity:"0",offset:"0%"}),o("stop",{stopColor:"#FFFFFF",offset:"100%"})]})]}),o("g",{id:"Page-1",stroke:"none",strokeWidth:"1",fill:"none",fillRule:"evenodd",children:o("g",{id:"Artboard",transform:"translate(-400.000000, -178.000000)",children:s("g",{id:"Group",transform:"translate(400.000000, 178.000000)",children:[o("path",{d:"M-5.68434189e-14,2.84217094e-14 L39.1816085,2.84217094e-14 L69.3453773,32.2519224 L101.428699,2.84217094e-14 L138.784583,2.84217094e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L6.71554594,44.4188507 C2.46876683,39.9813776 0.345377275,35.1089553 0.345377275,29.8015838 C0.345377275,24.4942122 0.230251516,14.560351 -5.68434189e-14,2.84217094e-14 Z",id:"Path",className:"text-primary",style:{fill:"currentColor"}}),o("path",{d:"M69.3453773,32.2519224 L101.428699,1.42108547e-14 L138.784583,1.42108547e-14 L138.784199,29.8015838 C137.958931,37.3510206 135.784352,42.5567762 132.260463,45.4188507 C128.736573,48.2809251 112.33867,64.5239941 83.0667527,94.1480575 L56.2750821,94.1480575 L32.8435758,70.5039241 L69.3453773,32.2519224 Z",id:"Path",fill:"url(#linearGradient-1)",opacity:"0.2"}),o("polygon",{id:"Path-2",fill:"#000000",opacity:"0.049999997",points:"69.3922914 32.4202615 32.8435758 70.5039241 54.0490008 16.1851325"}),o("polygon",{id:"Path-2",fill:"#000000",opacity:"0.099999994",points:"69.3922914 32.4202615 32.8435758 70.5039241 58.3683556 20.7402338"}),o("polygon",{id:"Path-3",fill:"url(#linearGradient-2)",opacity:"0.099999994",points:"101.428699 0 83.0667527 94.1480575 130.378721 47.0740288"})]})})})]}),r=o("button",{type:"button",className:"ms-1 btn-close"}),k=()=>s(d,{className:"demo-vertical-spacing",children:[o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"primary",children:"Vuexy"}),o(i,{children:"This is a toast with a primary icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"success",children:"Vuexy"}),o(i,{children:"This is a toast with a success icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"info",children:"Vuexy"}),o(i,{children:"This is a toast with a info icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"danger",children:"Vuexy"}),o(i,{children:"This is a toast with a danger icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"warning",children:"Vuexy"}),o(i,{children:"This is a toast with a warning icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:"dark",children:"Vuexy"}),o(i,{children:"This is a toast with a dark icon \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:o(u,{size:"sm",color:"primary"}),children:"Vuexy"}),o(i,{children:"This is a toast with a spinner \u2014 check it out!"})]})}),o(e,{md:"6",sm:"12",children:s(a,{children:[o(t,{close:r,icon:g,children:"Vuexy"}),o(i,{children:"This is a toast with a logo \u2014 check it out!"})]})})]}),b=o("pre",{children:o("code",{className:"language-jsx",children:`
+
+import { Toast, ToastBody, ToastHeader, Row, Col } from 'reactstrap'
+import transparentBg from '@src/assets/images/svg/transparent.svg'
+
+const close = (
+  <button type='button' className='ms-1 btn-close'>
+    <span>\xD7</span>
+  </button>
+)
+
+const ToastTranslucent = () => {
+  return (
+    <Row>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-primary my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a primary background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-secondary my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a secondary background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-success my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a success background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-danger my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a danger background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-warning my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a warning background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-info my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on an info background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+
+      <Col md='6' sm='12'>
+        <div className='p-3 bg-dark my-2 rounded'>
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on an dark background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+
+      <Col md='6' sm='12'>
+        <div
+          className='p-3 my-2 rounded'
+          style={{
+            background: "url(transparentBg)"
+          }}
+        >
+          <Toast>
+            <ToastHeader close={close}>Vuexy</ToastHeader>
+            <ToastBody>
+              This is a toast on a transparent background \u2014 check it out!
+            </ToastBody>
+          </Toast>
+        </div>
+      </Col>
+    </Row>
+  )
+}
+export default ToastTranslucent
+`})}),C=o("pre",{children:o("code",{className:"language-jsx",children:`
+
+import { Toast, ToastBody, ToastHeader, Spinner, Row, Col } from 'reactstrap'
+import logo from '@src/assets/images/logo/logo.png'
+
+const close = (
+  <button type='button' className='ms-1 btn-close'>
+    <span>\xD7</span>
+  </button>
+)
+
+
+const ToastHeaderIcons = () => {
+  return (
+    <Row>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='primary'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a primary icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='success'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a success icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='info'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a info icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='danger'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a danger icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='warning'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a warning icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon='dark'>Vuexy</ToastHeader>
+          <ToastBody>
+            This is a toast with a dark icon \u2014 check it out!
+          </ToastBody>
+        </Toast>
+      </Col>
+
+      <Col md='6' sm='12'>
+        <Toast>
+          <ToastHeader close={close} icon={<Spinner size='sm' color='primary' />}>
+            Vuexy
+          </ToastHeader>
+          <ToastBody>This is a toast with a spinner \u2014 check it out!</ToastBody>
+        </Toast>
+      </Col>
+
+      <Col md='6' sm='12'>
+        <Toast>
+        <ToastHeader close={close} icon={logo}>Vuexy</ToastHeader>
+          <ToastBody>This is a toast with a logo \u2014 check it out!</ToastBody>
+        </Toast>
+      </Col>
+    </Row>
+  )
+}
+export default ToastHeaderIcons
+`})}),w=()=>(n.exports.useEffect(()=>{T.highlightAll()},[]),s(n.exports.Fragment,{children:[o(m,{title:"Toasts",data:[{title:"Components"},{title:"Toasts"}]}),s(d,{className:"match-height",children:[o(e,{sm:"12",children:s(h,{title:"Toast Translucent",code:b,children:[o(l,{children:"Toasts are slightly translucent, too, so they blend over whatever they might appear over. For browsers that support the backdrop-filter CSS property, we\u2019ll also attempt to blur the elements under a toast."}),o(p,{})]})}),o(e,{sm:"12",children:s(h,{title:"Icons",code:C,children:[s(l,{children:["Use ",o("code",{children:"icon"})," attribute with ",o("code",{children:"ToastHeader"})," tag to add an icon."]}),o(k,{})]})})]})]}));export{w as default};
